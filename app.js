13d777541d99bcb67e5382655008964713c24864
jiasheng_ou <oujia6014@foxmail.com>
2019-05-30 12:00:42 +0800

    cookie操作

--- app.js
+++ app.js
@@ -40,6 +40,19 @@ const serverHandle = (req, res) => {
   //解析参数
   req.query = querystring.parse(url.split('?')[1]);
 
+  //解析客户端cookie
+  req.cookie = {}
+  const cookieStr = req.headers.cookie || ''; //key1=value1;k2=v2;...
+  cookieStr.split(';').forEach(item=>{
+    if(!item){
+      return
+    }
+    const arr = item.split('=')
+    const key = arr[0].trim()
+    const val = arr[1].trim()
+    req.cookie[key] = val
+  })
+
   getPostData(req).then(postData => {
     req.body = postData
     // 处理 blog 路由
--- src/router/user.js
+++ src/router/user.js
@@ -1,28 +1,56 @@
 const {loginCheck} = require('../controller/user')
+
+
+const getCookieExpires = (()=> {
+  const d = new Date()
+  d.setTime(d.getTime() + (24 * 40 * 60 * 1000))
+  console.error(d.toGMTString());
+  return d.toGMTString()
+})
+
 const handleUserRouter = (req, res) => {
   const method = req.method; //GET POST
   const url = req.url;
   const path = url.split('?')[0];
   // const query = req.query;
   //登录
-  if (method === 'POST' && path === '/api/user/login') {
-    const {username, passwork} = req.body
+  // if (method === 'POST' && path === '/api/user/login') {
+  //   const {username, passwork} = req.body
+  //   const result = loginCheck(username, passwork)
+  //   return result.then((data) => {
+  //     if(data.username){
+  //       data.state='登录成功'
+  //       return data
+  //     }else {
+  //       return '登录失败'
+  //     }
+  //   })
+  // }
+
+  //get登录
+  if (method === 'GET' && path === '/api/user/login') {
+    const {username, passwork} = req.query
     const result = loginCheck(username, passwork)
     return result.then((data) => {
       if(data.username){
-        data.state='登录成功'
+        data.state='登录成功:'+data.username;
+        res.setHeader('Set-Cookie',`username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`)//httpOnly 不允许前端改
         return data
       }else {
         return '登录失败'
       }
     })
-
-
-    // if (result) {
-    //   return '登录成功'
-    // } else {
-    //   return '登录失败'
-    // }
   }
+
+
+  if (method === 'GET' && path === '/api/user/cookie') {
+    if(req.cookie.username){
+      return  Promise.resolve('已有cookie登录成功'+req.cookie.username)
+    }
+    return  Promise.resolve('尚未登录')
+  }
+
+
+
 }
 module.exports = handleUserRouter
